39febe33345274e863e439de5a9c6a713fa7c576
Guo Yuhao <guoyuhao4@gmail.com>
2023-06-08 01:51:49 +0800

    setup
    This code should display 2 padded containers, separated by a white bar.
    
    In the first container it should simply be an image
    
    In the second container, 2/3 of it should be an image, a bit of white bar and the remaining are buttons.

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -19,20 +19,55 @@ class MyApp extends StatelessWidget {
           title: const Text("Welcome Back"), backgroundColor: Colors.green,
         ),
         body: Center(child: Column(children: <Widget>[
-            Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              color: Colors.amber,
-              border: Border.all(
-                width: 8,
-                color: Colors.red.shade200
-                ),
-              borderRadius: BorderRadius.circular(1)
-              ),
+            Expanded(
+              flex: 2,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.amber,
+                    border: Border.all(width: 8, color: Colors.red.shade200),
+                    borderRadius: BorderRadius.circular(1),
+                  ),
+                )
             ),
-            Container(
-
+          Expanded(
+            flex: 1,
+            child: SizedBox.expand(child: Text("Hello Blank Space"))
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child:
+              Column(children: <Widget>[
+                Expanded(
+                  flex: 10,
+                  child: Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.purple,
+                      border: Border.all(width: 4, color: Colors.brown.shade500),
+                      borderRadius: BorderRadius.circular(2),
+                    ),
+                  )
+                ),
+                Expanded(
+                  flex: 1,
+                  child: const SizedBox.expand()
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Row(children: <Widget>[
+                    TextButton(onPressed: () {}, child: const Text("Hi")),
+                    const SizedBox.expand(),
+                    ElevatedButton(onPressed: () {}, child: const Text("Float?"))
+                ])
             )
+              ])
+            )
+          )
           ]
         ),
         ),
